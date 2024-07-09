2c17d4a85635d3cb3fde0e54e4ea3469120b651a
Atashia Khan <atashia.khan@techieminions.co>
2024-07-09 16:32:05 +0500

    initial commit

--- quiz-app/get_input.rb
+++
@@ -1,7 +0,0 @@
-# module gets input from user and returns the input
-module GetInput
-  def self.call(str)
-    print str
-    gets.chomp
-  end
-end
--- quiz-app/user.rb
+++
@@ -1,8 +0,0 @@
-class User
-  attr_reader :name, :password
-
-  def initialize(name, password)
-    @name = name
-    @password = password
-  end
-end
